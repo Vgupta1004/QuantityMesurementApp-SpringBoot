0c2a7d090c447efc0ad76410b363a2faa7cb163a
gupta <vgup3012@7-11.com>
2026-03-26 12:31:48 +0530

    Initial Spring Boot Project Setup

---
+++ src/main/java/com/seveneleven/quantitymeasurement/QuantityMesurementAppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.seveneleven.quantitymeasurement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantityMesurementAppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantityMesurementAppSpringBootApplication.class, args);
+	}
+
+}
